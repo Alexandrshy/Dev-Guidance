b794c7b95188fb1e7612fa17072c23c091d947a3
Alex Shulaev <alexandrshy@gmail.com>
2023-08-20 00:21:27 +0400

    fix: fixed link

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -17,7 +17,7 @@ export default function Custom404() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="me" href="https://mastodon.social/@ctietze" />
+        <link rel="me" href="https://mastodon.social/@alexandrshy" />
       </Head>
       <main className={styles.main}>
         <Wrapper>
